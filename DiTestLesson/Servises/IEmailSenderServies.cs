0951918efb3ea01aa2d0345bc4efefafad519830
Denis <denshufly@mail.ru>
2019-11-24 19:55:36 +0600

    DiTest

---
+++ DiTestLesson/Servises/IEmailSenderServies.cs
@@ -0,0 +1,13 @@
+﻿using DiTestLesson.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiTestLesson.Servises
+{
+   public interface IEmailSenderServies
+    {
+        public Task SendEmail(EmailMessageDTO emailMessage);
+    }
+}
