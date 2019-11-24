61eddbd161fb8eb34072111cee3851b9354e4f16
Denis <denshufly@mail.ru>
2019-11-24 19:55:36 +0600

    DiTest

---
+++ DiTestLesson/Servises/IEntityServies.cs
@@ -0,0 +1,13 @@
+﻿using DiTestLesson.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiTestLesson.Servises
+{
+    public interface IEntityServies
+    {
+        Task SaveEntity(EntityDTO entity);
+    }
+}
---
+++ DiTestLesson/Servises/ISmsSenderServies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiTestLesson.Servises
+{
+    public interface ISmsSenderServies
+    {
+        public Task SendSms(string phoneNumber);
+    }
+}
